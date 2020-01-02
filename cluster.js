0b46f9b73534c990e93f9a3fa861c4d45c5cf59f
zhaoxianhao <zhaoxianhao321@163.com>
2020-01-02 17:47:27 +0800

    进程练习

---
+++ cluster.js
@@ -0,0 +1,19 @@
+const cluster = require('cluster');
+const os = require('os');
+const http = require('http');
+const process = require('process');
+if (cluster.isMaster) {
+	for (let i = 0; i < os.cpus().length; i++) {
+		cluster.fork();
+	}
+	console.log('我是主进程');
+} else {
+	// console.log('我是子进程');
+	console.log(process.pid);
+	let server=http.createServer((req,res)=>{
+		res.write('aa');
+		res.end();
+	}).listen(8080);
+	console.log("服务开好了");
+}
+
